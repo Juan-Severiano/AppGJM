b2074aae7669c01000ffb0ba2743bfae84aefa58
Francisco Juan <juansoussev@gmail.com>
2025-03-18 15:12:52 -0300

    feat: creating initial part of MedicineModel
    add correct types inside Medicines, with their

--- AppGJM/Medicines/MedicineModel.swift
+++ AppGJM/Medicines/MedicineModel.swift
@@ -2,6 +2,36 @@
 //  MedicineModel.swift
 //  AppGJM
 //
-//  Created by user on 17/03/25.
+//  Created by Francisco Juan on 17/03/25.
 //
 
+import Foundation
+
+enum Repetition: String, CaseIterable {
+    case every4Hours = "4 em 4 horas"
+    case every6Hours = "6 em 6 horas"
+    case every8Hours = "8 em 8 horas"
+    case every12Hours = "12 em 12 horas"
+}
+
+class MedicineModel {
+    let name: String
+    let quantity: Int
+    let typeOfMedicine: String
+    let format: String
+    var alarm: Bool
+    let firtTime: Date
+    let repetition: Repetition
+    let firstMedicineHour: Date
+    
+    init(name: String, quantity: Int, typeOfMedicine: String, format: String, alarm: Bool, firtTime: Date, repetition: Repetition, firstMedicineHour: Date) {
+        self.name = name
+        self.quantity = quantity
+        self.typeOfMedicine = typeOfMedicine
+        self.format = format
+        self.alarm = alarm
+        self.firtTime = firtTime
+        self.repetition = repetition
+        self.firstMedicineHour = firstMedicineHour
+    }
+}
